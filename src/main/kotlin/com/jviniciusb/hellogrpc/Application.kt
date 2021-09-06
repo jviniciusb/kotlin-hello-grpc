439aede52725b577faed4ba921bb935f91a4efe0
jviniciusb <jaderviniciusb@gmail.com>
2021-09-06 10:04:36 -0300

    Adjusted proto package

--- src/main/kotlin/com/jviniciusb/hellogrpc/Application.kt
+++ src/main/kotlin/com/jviniciusb/hellogrpc/Application.kt
@@ -5,6 +5,7 @@ import com.jviniciusb.hellogrpc.grpc.services.GreeterGrpcService
 
 fun main() {
     GrpcServerFactory.create(config = {
+        port = 6564
         useTransportSecurity = false
         services = listOf(GreeterGrpcService())
     }).start()
--- src/main/kotlin/com/jviniciusb/hellogrpc/grpc/services/GreeterGrpcService.kt
+++ src/main/kotlin/com/jviniciusb/hellogrpc/grpc/services/GreeterGrpcService.kt
@@ -1,16 +1,18 @@
 package com.jviniciusb.hellogrpc.grpc.services
 
-import com.jviniciusb.hellogrpc.proto.GreeterGrpcKt
+import com.jviniciusb.hellogrpc.proto.GreeterServiceGrpcKt
 import com.jviniciusb.hellogrpc.proto.HelloReply
 import com.jviniciusb.hellogrpc.proto.HelloRequest
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.transform
 
-class GreeterGrpcService : GreeterGrpcKt.GreeterCoroutineImplBase() {
+class GreeterGrpcService : GreeterServiceGrpcKt.GreeterServiceCoroutineImplBase() {
 
     override suspend fun sayHello(request: HelloRequest): HelloReply {
+        delay(COMPUTATION_SIMULATION_TIME)
         return HelloReply.newBuilder().setMessage("Hello ${request.name}!").build()
     }
 
@@ -24,15 +26,22 @@ class GreeterGrpcService : GreeterGrpcKt.GreeterCoroutineImplBase() {
 
     override fun sayStreamHello(request: HelloRequest): Flow<HelloReply> = flow {
         repeat(5) {
+            delay(COMPUTATION_SIMULATION_TIME)
             emit(HelloReply.newBuilder().setMessage("Hello ${request.name} $it!").build())
         }
     }
 
     override fun sayStreamHelloToStream(requests: Flow<HelloRequest>): Flow<HelloReply> {
         return requests.transform { request ->
-            repeat(5) {
+            repeat(2) {
+                delay(COMPUTATION_SIMULATION_TIME)
                 emit(HelloReply.newBuilder().setMessage("Hello ${request.name} $it!").build())
             }
         }
+    }
+
+    private companion object {
+        // Value in milliseconds to be used with dealy() to simulates some computation
+        const val COMPUTATION_SIMULATION_TIME = 1000L
     }
 }
