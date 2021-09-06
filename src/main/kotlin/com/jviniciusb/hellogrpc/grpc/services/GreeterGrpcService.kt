c2b3113a4511df8fd6546d4f4be5527bde344548
jviniciusb <jaderviniciusb@gmail.com>
2021-09-06 10:25:36 -0300

    Adjusted proto package

--- src/main/kotlin/com/jviniciusb/hellogrpc/grpc/services/GreeterGrpcService.kt
+++ src/main/kotlin/com/jviniciusb/hellogrpc/grpc/services/GreeterGrpcService.kt
@@ -1,41 +1,40 @@
 package com.jviniciusb.hellogrpc.grpc.services
 
-import com.jviniciusb.hellogrpc.proto.GreeterServiceGrpcKt
-import com.jviniciusb.hellogrpc.proto.HelloReply
-import com.jviniciusb.hellogrpc.proto.HelloRequest
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.transform
+import proto.greeter.v1.Greeter
+import proto.greeter.v1.GreeterServiceGrpcKt
 
 class GreeterGrpcService : GreeterServiceGrpcKt.GreeterServiceCoroutineImplBase() {
 
-    override suspend fun sayHello(request: HelloRequest): HelloReply {
+    override suspend fun sayHello(request: Greeter.HelloRequest): Greeter.HelloReply {
         delay(COMPUTATION_SIMULATION_TIME)
-        return HelloReply.newBuilder().setMessage("Hello ${request.name}!").build()
+        return Greeter.HelloReply.newBuilder().setMessage("Hello ${request.name}!").build()
     }
 
-    override suspend fun sayHelloToStream(requests: Flow<HelloRequest>): HelloReply {
+    override suspend fun sayHelloToStream(requests: Flow<Greeter.HelloRequest>): Greeter.HelloReply {
         val names = mutableListOf<String>()
         requests.collect {
             names.add(it.name)
         }
-        return HelloReply.newBuilder().setMessage("Hello ${names.joinToString()}!").build()
+        return Greeter.HelloReply.newBuilder().setMessage("Hello ${names.joinToString()}!").build()
     }
 
-    override fun sayStreamHello(request: HelloRequest): Flow<HelloReply> = flow {
+    override fun sayStreamHello(request: Greeter.HelloRequest): Flow<Greeter.HelloReply> = flow {
         repeat(5) {
             delay(COMPUTATION_SIMULATION_TIME)
-            emit(HelloReply.newBuilder().setMessage("Hello ${request.name} $it!").build())
+            emit(Greeter.HelloReply.newBuilder().setMessage("Hello ${request.name} $it!").build())
         }
     }
 
-    override fun sayStreamHelloToStream(requests: Flow<HelloRequest>): Flow<HelloReply> {
+    override fun sayStreamHelloToStream(requests: Flow<Greeter.HelloRequest>): Flow<Greeter.HelloReply> {
         return requests.transform { request ->
             repeat(2) {
                 delay(COMPUTATION_SIMULATION_TIME)
-                emit(HelloReply.newBuilder().setMessage("Hello ${request.name} $it!").build())
+                emit(Greeter.HelloReply.newBuilder().setMessage("Hello ${request.name} $it!").build())
             }
         }
     }
